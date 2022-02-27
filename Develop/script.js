cf6186d1f77d855aff65b9a3ef9134c1b66a5449
charanvir <charanvir123@gmail.com>
2022-02-26 19:54:30 -0500

    added criteria to pick from to increase array size

--- Develop/script.js
+++ Develop/script.js
@@ -19,24 +19,55 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   var passWordLength = prompt("Please select a password length between 8 and 128");
-  while (passWordLength > 128 || passWordLength < 8) {
+  while (passWordLength > 128 || passWordLength < 8 || passWordLength === null) {
     alert("Password must be between 8 and 128 characters")
     var passWordLength = prompt("Please select a password length between 8 and 128");
   }
-  var password = "";
+  var includeUpperCase = window.confirm("Would you like to include upper case letters? Press Ok to include and cancel to not include")
+  if (includeUpperCase) {
+
+    password = allCharacters.push(upperCase);
+  } else {
+    password = allCharacters;
+  };
 
 
+  var includeLowerCase = window.confirm("Would you like to include lower case letters? Press Ok to include and cancel to not include")
+  if (includeLowerCase) {
 
-  for (var i = 0; i < passWordLength; i++)
-    password += fullDataBase.charAt(Math.floor(Math.random() * fullDataBase.length));
+    password = allCharacters.push(lowerCase);
+  } else {
+    password = allCharacters;
+  };
+
+
+  var includeNumbers = window.confirm("Would you like to include numbers? Press Ok to include and cancel to not include")
+  if (includeNumbers) {
+
+    password = allCharacters.push(numbers);
+  } else {
+    password = allCharacters;
+  };
+
+
+  var includeSpecialCharacters = window.confirm("Would you like to include special characters? Press Ok to include and cancel to not include")
+  if (includeSpecialCharacters) {
+
+    password = allCharacters.push(specialCharacters);
+  } else {
+    password = allCharacters;
+  };
+
 
 
   return password;
+
 }
 
 
-var fullDataBase = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var upperCaseOnly = "ABCDEFGHIJKLMOPWRSTUVWXYZ";
-var lowerCaseOnly = 'abcedfghijklmnopqrstuvwxyz';
-var numbersOnly = "0123456789";
-var specialCharactersOnly = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+var allCharacters = ["HELLO"];
+
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var specialCharacters = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-'];
